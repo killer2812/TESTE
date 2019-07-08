b3592345980c9fe544686b4ebe5e0ca4cd831606
Marcos Warmling <marcos_h_warmling@estudante.sc.senai.br>
2019-07-08 16:56:15 -0300

    add logo e tentativa fracassada de salvar no local storage

--- js/main.js
+++ js/main.js
@@ -5,13 +5,22 @@ function adicionaLinha(idTabela) {
     var celula1 = linha.insertCell(0);
     var celula2 = linha.insertCell(1);
     var celula3 = linha.insertCell(2);
+    var celula4 = linha.insertCell(3);
+    var celula5 = linha.insertCell(4);
     celula1.innerHTML = prompt("produto")
-    celula2.innerHTML = prompt("valor")
-    celula3.innerHTML = "<button onclick='removeLinha(this)'>Remover</button>";
+    celula2.innerHTML = prompt("quantidade")
+    celula3.innerHTML = prompt("preço de compra")
+    celula4.innerHTML = prompt("preço de venda")
+    celula5.innerHTML = "<button onclick='removeLinha(this)';>Del</button>";
+  
 }
 
 
 function removeLinha(linha) {
     var i = linha.parentNode.parentNode.rowIndex;
     document.getElementById('tbl').deleteRow(i);
+    saveTostorage()
 } 
+celula1 = localStorage.getItem('celula1');
+localStorage.setItem('celula1', celula1)
+
