b329d4ff592b6bc5807865b79c8f9b2f21dfd289
Marcos Warmling <marcos_h_warmling@estudante.sc.senai.br>
2019-09-02 21:36:44 -0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,26 +1,38 @@
-function adicionaLinha(idTabela) {
-    var tabela = document.getElementById(idTabela);
-    var numeroLinhas = tabela.rows.length;
-    var linha = tabela.insertRow(numeroLinhas);
-    var celula1 = linha.insertCell(0);
-    var celula2 = linha.insertCell(1);
-    var celula3 = linha.insertCell(2);
-    var celula4 = linha.insertCell(3);
-    var celula5 = linha.insertCell(4);
-    celula1.innerHTML = prompt("produto")
-    celula2.innerHTML = prompt("quantidade")
-    celula3.innerHTML = prompt("preço de compra")
-    celula4.innerHTML = prompt("preço de venda")
-    celula5.innerHTML = "<button onclick='removeLinha(this)';>Del</button>";
-  
-}
+function convertTemp(direction) {
+    //captura objetos
+    var fire = document.convert.ftemp
+    var grau = document.convert.ctemp
+    var ku = document.convert.ktemp
+
+    //faz as conversões
+
+    
+    if (direction == "fire") {
+        grau.value = Math.round((fire.value - 32) * (5 / 9));
+        ku.value = Math.round((parseFloat(grau.value) + 459.67) * (5 / 9));
+      
+
+        return
+    }else if (direction == "ku") {
+        fire.value = Math.round((parseFloat(grau.value) * (9 / 5)) - 459.67);
+        grau.value = Math.round((fire.value - 32) * (5 / 9));
+        
+return
+    }   else 
+        fire.value = Math.round((parseFloat(grau.value) * (9 / 5)) + 32);
+        ku.value = Math.round((parseFloat(grau.value) + 273));
+return
+  }
+
+ 
+ 
 
 
-function removeLinha(linha) {
-    var i = linha.parentNode.parentNode.rowIndex;
-    document.getElementById('tbl').deleteRow(i);
-    saveTostorage()
-} 
-celula1 = localStorage.getItem('celula1');
-localStorage.setItem('celula1', celula1)
+
+
+
+
+
+
+
 
